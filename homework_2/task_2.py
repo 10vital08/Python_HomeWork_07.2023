0dae3363b65cdee2326cd9fa3b7cf2e56ecbc313
10vital08 <vital.lushnikov@mail.ru>
2023-07-05 21:33:01 +0300

    completed task 2

--- homework_2/task_2.py
+++ homework_2/task_2.py
@@ -3,6 +3,12 @@
 # для проверки своего результата.
 
 HEXADECIMAL_SYSTEM = 16
+A = 'A'
+B = 'B'
+C = 'C'
+D = 'D'
+E = 'E'
+F = 'F'
 
 def transfer_to_the_system(number: int, system: int) -> str:
     result_sys_16: str = ''
@@ -10,7 +16,21 @@ def transfer_to_the_system(number: int, system: int) -> str:
 
     while number:
         mod = str(number % system)
-        result_sys_16 = mod + result_sys_16
+        if mod == '10':
+            result_sys_16 = A + result_sys_16
+        elif mod == '11':
+            result_sys_16 = B + result_sys_16
+        elif mod == '12':
+            result_sys_16 = C + result_sys_16
+        elif mod == '13':
+            result_sys_16 = D + result_sys_16
+        elif mod == '14':
+            result_sys_16 = E + result_sys_16
+        elif mod == '15':
+            result_sys_16 = F + result_sys_16
+        else:
+            result_sys_16 = mod + result_sys_16
+
         number //= system
     return result_sys_16
 
