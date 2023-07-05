af5b33e301f1f9c80a21ed6942759e6495b3c9ba
10vital08 <vital.lushnikov@mail.ru>
2023-07-05 21:14:28 +0300

    Почти доделал вторую

--- homework_2/task_2.py
+++ homework_2/task_2.py
@@ -2,3 +2,20 @@
 # шестнадцатеричное строковое представление. Функцию hex используйте
 # для проверки своего результата.
 
+HEXADECIMAL_SYSTEM = 16
+
+def transfer_to_the_system(number: int, system: int) -> str:
+    result_sys_16: str = ''
+    mod: str = ''
+
+    while number:
+        mod = str(number % system)
+        result_sys_16 = mod + result_sys_16
+        number //= system
+    return result_sys_16
+
+number: int = int(input('Введите целое число: '))
+res_str: str = transfer_to_the_system(number, HEXADECIMAL_SYSTEM)
+
+print(f"16-ричное представление с помощью моей функции = {res_str}")
+print(f"16-ричное представление с помощью встроенной функции = {hex(number)[2:]}")
