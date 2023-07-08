70a0a81af76bfc7a83a909b5c90be9721ba9ce65
10vital08 <vital.lushnikov@mail.ru>
2023-07-08 09:33:05 +0300

    completed task_2

--- homework_3/task_2.py
+++ homework_3/task_2.py
@@ -2,3 +2,11 @@
 # с дублирующимися элементами. В результирующем списке
 # не должно быть дубликатов.
 
+list_1 = [1, 2, 1, 2, 2, 5, 3, 6, 7, 7, 8, 2]
+list_result = []
+
+for i in list_1:
+    if list_1.count(i) > 1 and i not in list_result:
+        list_result.append(i)
+
+print(list_result)
