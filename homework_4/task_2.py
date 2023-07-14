7061dbc48b597f09045fd13a0bf2add09897e254
10vital08 <vital.lushnikov@mail.ru>
2023-07-14 19:39:48 +0300

    completed task 2

--- homework_4/task_2.py
+++ homework_4/task_2.py
@@ -3,3 +3,20 @@
 # а значение — имя аргумента. Если ключ не хешируем, используйте его строковое представление.
 
 
+def return_dictonary(**kwargs):
+    res_dict = {}
+
+    for key, value in kwargs.items():
+        if hash(value):
+            value
+        else:
+            value = ''.join(value)
+
+        res_dict[value] = key
+
+    return res_dict
+
+
+dict_result = return_dictonary(математика=5, физика=4)
+
+print(dict_result)
