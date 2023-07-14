0107dc47133956646a9b067a7d431792a3e7a822
10vital08 <vital.lushnikov@mail.ru>
2023-07-14 16:40:52 +0300

    completed task 1 h4

--- homework_4/task_1.py
+++ homework_4/task_1.py
@@ -1,9 +1,19 @@
 # Напишите функцию для транспонирования матрицы
 
 
+from copy import deepcopy
+
+
 def matrix_transposition(matr):
-    result_matrix = matr
-    print(result_matrix)
+    """Транспонирование матрицы"""
+
+    # копирование матрицы
+    result_matrix = deepcopy(matr)
+
+    for i in range(len(matr)):
+        for j in range(len(matr[0])):
+            result_matrix[j][i] = matr[i][j]
+
     return result_matrix
 
 
@@ -11,4 +21,11 @@ matrix = [[1, 2, 5],
           [2, 7, 8],
           [87, 66, 3]]
 
+for i in matrix:
+    print(i)
+
 trans_matrix = matrix_transposition(matrix)
+
+print('')
+for i in trans_matrix:
+    print(i)
