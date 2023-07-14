200d815c61c920ca3f45c85b6a4895796a75dd7e
10vital08 <vital.lushnikov@mail.ru>
2023-07-14 16:17:09 +0300

    open

---
+++ homework_4/task_1.py
@@ -0,0 +1,14 @@
+# Напишите функцию для транспонирования матрицы
+
+
+def matrix_transposition(matr):
+    result_matrix = matr
+    print(result_matrix)
+    return result_matrix
+
+
+matrix = [[1, 2, 5],
+          [2, 7, 8],
+          [87, 66, 3]]
+
+trans_matrix = matrix_transposition(matrix)
