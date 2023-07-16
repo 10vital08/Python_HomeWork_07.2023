d7ea3b3981bebd53eddc426057c9805a720ec4f2
10vital08 <vital.lushnikov@mail.ru>
2023-07-16 17:12:18 +0300

    completed task 2-3

--- homework_4/task_2.py
+++ homework_4/task_2.py
@@ -7,16 +7,14 @@ def return_dictonary(**kwargs):
     res_dict = {}
 
     for key, value in kwargs.items():
-        if hash(value):
-            value
-        else:
-            value = ''.join(value)
+        if not isinstance(value, (int, float, str, tuple, frozenset)):
+            value = str(value)
 
         res_dict[value] = key
 
     return res_dict
 
 
-dict_result = return_dictonary(математика=5, физика=4)
+dict_result = return_dictonary(математика=[1, 2], физика=4)
 
 print(dict_result)
