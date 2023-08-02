6e7c5718a63d8f30169038d51edbfbdea85f9c92
10vital08 <vital.lushnikov@mail.ru>
2023-08-02 14:20:24 +0300

    final

---
+++ homework_9/__init__.py
@@ -0,0 +1,8 @@
+# 3. Соберите пакет с играми из тех файлов, что уже были созданы в рамках курса
+
+from task_2_9 import *
+from sem_9_4 import *
+from sem_9_5_and_6 import *
+
+__all__ = ['deco_csv', 'json_result', 'create_coefficients_csvfile', 'solve_square_equation',
+           'counter', 'test', 'game_decorator', 'counter', 'json_cache']
