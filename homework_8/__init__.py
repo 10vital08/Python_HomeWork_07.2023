ef27e5924e9822d8ffa44cbc8b1410797c1511d5
10vital08 <vital.lushnikov@mail.ru>
2023-08-02 14:20:51 +0300

    8

---
+++ homework_8/__init__.py
@@ -0,0 +1,6 @@
+from task_2_8 import *
+from task_seminare_5 import *
+from task_seminare_6 import *
+from task_seminare_7 import *
+
+__all__ = ['write_json_csv_pickle', 'direct', 'pickle2csv', 'pic_in_csv']
